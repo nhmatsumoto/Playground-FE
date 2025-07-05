5c2f70a6571d33b71130de9914d1884d4db3bacb
Hiro Matsumoto <hiroyukims@hotmail.com>
2025-07-05 23:14:56 +0900

    updates

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,7 +1,7 @@
 const Header = () => {
     return (
         <header className="bg-gray-800 text-white p-4">
-            <h1 className="text-xl">My Application</h1>
+            <h1 className="text-xl"></h1>
         </header>
     )
 }
